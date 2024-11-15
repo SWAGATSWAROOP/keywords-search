2e61808099a5b290f6317a8931007b4de8d5c9fc
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-11-15 18:48:51 +0530

    Added 2nd agent route

--- index.js
+++ index.js
@@ -82,6 +82,63 @@ app.get("/getkeyword", async (req, res) => {
   }
 });
 
+app.post("/generatecontent", async (req, res) => {
+  try {
+    const arrayKeyword = req.body.keywords;
+    const joinedString = arrayKeyword.join(" ");
+    // console.log(joinedString);
+    // console.log(process.env.ondemand_api_key);
+
+    async function getChatSession() {
+      const options = {
+        method: "POST",
+        url: "https://api.on-demand.io/chat/v1/sessions",
+        headers: {
+          accept: "application/json",
+          "content-type": "application/json",
+          apikey: process.env.ondemand_api_key,
+        },
+        data: {
+          externalUserId: "HackBrokers",
+        },
+      };
+
+      const response = await axios(options);
+      return response?.data?.data?.id;
+    }
+
+    const getNewsChat = async (arrayKeyword) => {
+      const sessionid = await getChatSession();
+      console.log(sessionid);
+
+      const options = {
+        method: "POST",
+        url: `https://api.on-demand.io/chat/v1/sessions/${sessionid}/query`,
+        headers: {
+          accept: "application/json",
+          "content-type": "application/json",
+          apikey: process.env.ondemand_api_key,
+        },
+        data: {
+          responseMode: "sync",
+          query: `Provide 1500 word article on ${arrayKeyword} which is SEO friendly`,
+          onlyFulfillment: true,
+          endpointId: "predefined-openai-gpt4o",
+        },
+      };
+
+      const response = await axios(options);
+      console.log(response.data);
+      return response.data?.data?.answer;
+    };
+    // console.log(joinedString);
+    const answer = await getNewsChat(joinedString);
+    return res.status(200).json({ answer });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server Listening on port 3000");
 });
